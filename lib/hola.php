7d79078a62355d371bac683bd8a17ae5d9e0b049
anaissoriano <anaisclase13@gmail.com>
2024-02-01 19:14:22 +0100

    Programa interactivo

--- lib/hola.php
+++ lib/hola.php
@@ -1,6 +1,5 @@
 <?php
 // Autor: Anais Soriano <anaisclase13@gmail.com>
-// El nombre por defecto es Anais Soriano
 $nombre = isset($argv[1]) ? $argv[1] : "Anais Soriano";
 @print "Hola {$nombre}\n";
 ?>
